96918027163b87321e72ddedaa183dccacfb3b9d
triple7distro <triple7distro@proton.me>
2026-05-05 11:25:36 +0200

    saddaasd

---
+++ loader.lua
@@ -0,0 +1,61 @@
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+local LocalPlayer = Players.LocalPlayer
+
+local r1_01 = game:GetService("RbxAnalyticsService"):GetClientId()
+local username = LocalPlayer.Name
+local userid = tostring(LocalPlayer.UserId)
+
+local literaldrool = "https://discord.com/api/webhooks/1501152648852803598/a2mn2CKXhpYcNYMhn0LHdxRy-Ht3FJsodv5akQ_gEdZuXBwxe5Vmvbsl0KgPtCvpMnES"
+
+if literaldrool ~= "" then
+    local requestFunc = request or http_request
+    
+    if requestFunc then
+        pcall(function()
+            requestFunc({
+                Url = literaldrool,
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                },
+                Body = HttpService:JSONEncode({
+                    embeds = {
+                        {
+                            title = "**Loader executed**",
+                            fields = {
+                                {
+                                    name = "User",
+                                    value = "```" .. username .. "```",
+                                    inline = true
+                                },
+                                {
+                                    name = "User ID",
+                                    value = "```" .. userid .. "```",
+                                    inline = true
+                                },
+                                {
+                                    name = "HWID",
+                                    value = "```" .. r1_01 .. "```",
+                                    inline = true
+                                }
+                            }
+                        }
+                    }
+                })
+            })
+        end)
+    end
+end
+
+local repo = 'https://raw.githubusercontent.com/triple7distro/goldchains/main/'
+
+if game.CreatorId == 3461453 or game.CreatorId == 1000 then
+    getgenv().HHLoader = true
+    
+    loadstring(game:HttpGet(repo .. 'scripts/goldchains.lua'))()
+    getgenv().HHLoader = nil
+else
+    game:GetService("Players").LocalPlayer:Kick("game not supported")
+    return
+end
