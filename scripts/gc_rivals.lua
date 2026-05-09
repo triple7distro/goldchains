e666f1167a4d9264cf7502f532aea01f41d3e514
triple7distro <triple7distro@proton.me>
2026-05-09 23:23:38 +0200

    asdasdasdads

--- scripts/gc_rivals.lua
+++ scripts/gc_rivals.lua
@@ -1,54 +1,5 @@
 if not getgenv().litteringenv then
-    local Players = game:GetService("Players")
-    local HttpService = game:GetService("HttpService")
-    local LocalPlayer = Players.LocalPlayer
-    
-    local r1_01 = game:GetService("RbxAnalyticsService"):GetClientId()
-    local username = LocalPlayer.Name
-    local userid = tostring(LocalPlayer.UserId)
-    
-    local r1_02 = "https://discord.com/api/webhooks/1501152648852803598/a2mn2CKXhpYcNYMhn0LHdxRy-Ht3FJsodv5akQ_gEdZuXBwxe5Vmvbsl0KgPtCvpMnES"
-    
-    if r1_02 ~= "" then
-        local requestFunc = request or http_request
-        if requestFunc then
-            pcall(function()
-                requestFunc({
-                    Url = r1_02,
-                    Method = "POST",
-                    Headers = {
-                        ["Content-Type"] = "application/json"
-                    },
-                    Body = HttpService:JSONEncode({
-                        embeds = {
-                            {
-                                title = "**Loader bypass attempted**",
-                                fields = {
-                                    {
-                                        name = "User",
-                                        value = "```" .. username .. "```",
-                                        inline = true
-                                    },
-                                    {
-                                        name = "User ID",
-                                        value = "```" .. userid .. "```",
-                                        inline = true
-                                    },
-                                    {
-                                        name = "HWID",
-                                        value = "```" .. r1_01 .. "```",
-                                        inline = true
-                                    }
-                                }
-                            }
-                        }
-                    })
-                })
-            end)
-        end
-    end
-    
-    LocalPlayer:Kick("bypassing loader huh?")
+    game:GetService("Players").LocalPlayer:Kick("bypassing loader huh?")
     return
 end
 
